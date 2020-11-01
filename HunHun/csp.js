4a0bf1c7e537ac2fc1827387929681e7abe49ddc
K-Rintaro <70018855+K-Rintaro@users.noreply.github.com>
2020-11-01 22:00:40 +0900

    support http site, too.

--- HunHun/csp.js
+++ HunHun/csp.js
@@ -1,15 +1,16 @@
-var targetPage = "https://*/*";
+var targetPage1 = "https://*/*";
+var targetPage2 = "http://*/*"
 
 function AddSecureCSP(e) {
   e.requestHeaders.forEach(function(header){
       header.name = "Content-Security-Policy";
-      header.value = "block-all-mixed-content"
+      header.value = "block-all-mixed-content'"
   });
   return {requestHeaders: e.requestHeaders};
 }
 
 chrome.webRequest.onBeforeSendHeaders.addListener(
   AddSecureCSP,
-  {urls: [targetPage]},
+  {urls: [targetPage1, targetPage2]},
   ["blocking", "requestHeaders"]
 );
