a1ee8475828d7567f34723fc3c9346e942a2b39d
K-Rintaro <git.rintaro@gmail.com>
2020-11-10 23:30:49 +0900

    Update csp.js

--- HunHun/csp.js
+++ HunHun/csp.js
@@ -4,7 +4,7 @@ var targetPage2 = "http://*/*"
 function AddSecureCSP(e) {
   e.requestHeaders.forEach(function(header){
       header.name = "Content-Security-Policy";
-      header.value = "block-all-mixed-content'"
+      header.value = "block-all-mixed-content"
   });
   return {requestHeaders: e.requestHeaders};
 }
